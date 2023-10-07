7e9c1786e1078a26427ba129f570f2c32b4c970b
xuyang0602 <wang.xuyang0602@gmail.com>
2023-10-07 00:13:39 -0700

    Add get vendors method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
+import { Vendors } from '@/api/types';
+import { getVendors } from '@/api/vendors';
 import Main from '@/components/main';
 import Head from 'next/head';
-export default function Home() {
+
+interface HomeProps {
+  initVendors: Vendors;
+}
+
+export default function Home({ initVendors }: HomeProps) {
+  console.log(initVendors);
   return (
     <>
       <Head>
@@ -15,4 +23,26 @@ export default function Home() {
       <Main />
     </>
   );
+}
+
+export async function getStaticProps() {
+  let vendors: Vendors | Error;
+
+  try {
+    vendors = await getVendors<Vendors>(10);
+  } catch (e) {
+    if (e instanceof Error) {
+      // Return empty vendors
+      vendors = { Items: [], count: 0, lastEvaluatedKey: null };
+    } else {
+      throw new Error('getVendors unexpected Error');
+    }
+  }
+
+  return {
+    props: {
+      initVendors: vendors,
+    },
+    revalidate: 60, // revalidate the cache
+  };
 }
