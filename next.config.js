96faf2517f231893fc7553f2784afeea6cd13bae
xuyang0602 <wang.xuyang0602@gmail.com>
2023-10-06 22:20:00 -0700

    Add configuration for next js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  compiler: {
+    styledComponents: {
+      // Enable display of the component name along with the generated className (needed for debugging).
+      displayName: true,
+
+      // Enable SSR support
+      ssr: true,
+
+      // Optional
+      fileName: false,
+    },
+  },
 };
 
 module.exports = nextConfig;
